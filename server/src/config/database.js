32a13db0122bc43cbde7f5c7cdfe2f11c89e53cc
Josue619 <josue.martinez.cz@gmail.com>
2020-09-24 21:25:38 -0600

    Cambios

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.MYSQL_DB, process.env.DB_USER, '',
+const sequelize = new Sequelize(process.env.MYSQL_DB, process.env.DB_USER, process.env.DB_PASS,
     {
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
