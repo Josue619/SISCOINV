dd555840d9fbef5c061806fb8036b47f507ee72b
jairosv <60631616+jairosv@users.noreply.github.com>
2020-11-09 20:15:46 -0600

    Se quitaron los estilos

--- client/src/components/layout/Sidebar.js
+++ client/src/components/layout/Sidebar.js
@@ -37,7 +37,7 @@ const Sidebar = () => {
                     
                 </ul>
             </div>
-            <hr className="my-4" />
+            <hr className="my-4"/>
             
             
             <h1>Procesos</h1>
--- server/src/database/RoleDB.js
+++ server/src/database/RoleDB.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const db = require('../config/database');
 
-var nameTable = 'seg_rol';
+var nameTable = 'seg_rols';
 var Role = db.define(nameTable, {
     ROL_DESCRIPCION: Sequelize.STRING
 },
